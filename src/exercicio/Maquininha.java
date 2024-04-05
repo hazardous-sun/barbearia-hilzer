eb1c7019236e431f55aa8e96cc2ced578f161b99
solaire-notebook <contact@unharmfulhazard.com>
2024-04-05 09:41:49 -0300

    Implemented the run method for Maquininha

--- src/exercicio/Maquininha.java
+++ src/exercicio/Maquininha.java
@@ -1,7 +1,23 @@
 package exercicio.src.exercicio;
 
 public class Maquininha extends Thread {
-    public synchronized void cobrarCliente(Barbeiro barbeiro, Cliente cliente) {
+    public synchronized void cobrarCliente(Cliente cliente) {
         System.out.println("Cliente sendo cobrado.");
+        try {
+            sleep((int) (Math.random() * Math.random()));
+        } catch (InterruptedException e) {
+            System.out.println(e);
+        }
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                sleep(10000);
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
+        }
     }
 }
