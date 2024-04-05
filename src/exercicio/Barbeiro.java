733d6bfa8a861c67f4dcb4e3c17b46ac5ae03b43
solaire-notebook <contact@unharmfulhazard.com>
2024-04-05 09:38:43 -0300

    Added the run method for Barbeiro

--- src/exercicio/Barbeiro.java
+++ src/exercicio/Barbeiro.java
@@ -1,15 +1,19 @@
 package exercicio.src.exercicio;
 public class Barbeiro extends Thread {
-    public void atenderCliente(Cliente cliente) {
-        System.out.println("Cliente sendo atendido");
-        try {
-            sleep((int) (Math.random() * Math.random()));
-        } catch (InterruptedException e) {
-            System.out.println(e);
+    private void atenderCliente(Barbearia barbearia) {
+        while (barbearia.totalPopulation() > 0) {
+            Cliente cliente = barbearia.chamarCliente();
+            System.out.println("Cliente sendo atendido");
+            try {
+                sleep((int) (Math.random() * Math.random()));
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
+            barbearia.pegarMaquinha(cliente);
         }
     }
 
-//    public void run(Barbearia barbearia) {
-//        atenderCliente();
-//    }
+    public void run(Barbearia barbearia) {
+        atenderCliente(barbearia);
+    }
 }
