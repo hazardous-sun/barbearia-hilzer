a89238303e1b1e9e20cdfb8e7ae6b4f25491852e
Mateus Wolke <61051738+wolke412@users.noreply.github.com>
2024-04-15 11:59:04 -0300

    Cleaning repo

--- Cliente.java
+++
@@ -1,9 +0,0 @@
-package exercicio;
-
-public class Cliente extends Thread {
-    private long horaChegada;
-
-    Cliente(Barbearia barbearia) {
-        this.horaChegada = System.nanoTime();
-    }
-}
