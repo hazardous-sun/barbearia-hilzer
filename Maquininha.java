860d1a68164ada5617f0ec0384620acbd108ecad
Mateus Wolke <61051738+wolke412@users.noreply.github.com>
2024-04-15 11:58:50 -0300

    Cleaning repo

--- Maquininha.java
+++
@@ -1,8 +0,0 @@
-package exercicio;
-
-public class Maquininha extends Thread {
-    public synchronized void cobrarCliente(Barbeiro barbeiro, Cliente cliente) {
-        System.out.println("Cliente sendo cobrado.");
-
-    }
-}
